5dd6e665cb0196448dc678a73e152d75298fef3a
emperorPrki <chaboggg@gmail.com>
2022-07-04 21:03:03 +0200

    Fix naming, add game() function
    Fix function and variable names to be in accordance with the TOP project
    guidelines
    Add game() function
    Add condition which checks if weapons are valid, if not prompts player to
    choose a valid weapon before continuing
    Update PSEUDO-CODE

--- js/rps.js
+++ js/rps.js
@@ -2,15 +2,15 @@
 
 PSEUDO CODE:
 Prompt user to userPick a weapon                                ++
-    - convert string to uppercase or lowercase for evaluation   ++
+    - make choice case insensitive                              ++
     - Three choices: Rock, Paper, Scissors                      ++
-    - Accept only one of those three choices                    To be solved
+    - Accept only one of those three choices                    ++
 
 Randomize opponents weapon choice                               ++
     - randomize a number between 1 and 3                        ++
     - convert randomized number to weapon choice                ++
     - convert weapon choice to a string                         ++
-    - convert string to uppercase or lowercase for evaluation   To be solved
+    - make choice case insensitive                              ++
 
 Parse choices to evaluation                                     ++
 
@@ -32,66 +32,109 @@ Evaluate the choices                                            ++
     }
     
 Add point to score                                               ++
-    - if userPick wins, add point to user                        To be solved
-    - if computerPick wins, add point to computer                To be solved
+    - if userPick wins, add point to user                        ++
+    - if computerPick wins, add point to computer                ++
     - if tie, no added points                                    ++
 
+Add game function that combines all previous functions to form a game  ++
+    - Have 5 rounds                                              ++
+    - At start of each game reset counters to 0                  ++
+    - At the end of round 5 announce game winners                ++
 */
 
-//Prompt a user to pick a weapon
 
-let userPick;
-let computerPick;
+let playerSelection;
+let computerSelection;
+let playerPoints = 0;
+let computerPoints = 0;
+let weapons = ["rock", "paper", "scissors"];
 
-function chooseWeaponUser(){
-
-    userPick = prompt("Choose a weapon of choice", "");
-    console.log("User picked: " + userPick);
-    return userPick;
+//Capitalizes a string
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-//Randomizes computer weapon using numbers 1 - 3;
-function chooseWeaponComputer(){
+//Gets weapon of choice input from player.
+function playerPlay() {
+
+    playerSelection = prompt("Choose a weapon of choice", "");
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection !== weapons[0] && playerSelection !== weapons[1] && playerSelection !== weapons[2]){
+        console.log("Choose a valid weapon!")
+        playerPlay();
+    } else {
+        console.log("User picked: " + capitalize(playerSelection));
+        return playerSelection;
+    }
+    
+}
+
+//Gets computers weapon of choice using randomized numbers 1 - 3;
+function computerPlay() {
 
     let randomNumber = Math.floor(Math.random() * 3 + 1);
-    console.log(randomNumber);
-    let weapons = ["rock", "paper", "scissors"];
     if (randomNumber === 1){
-        computerPick = weapons[0];
-        console.log("Computer picked: " + computerPick);
-        return computerPick;
+        computerSelection = weapons[0];
+        console.log("Computer picked: " + capitalize(computerSelection));
+        return computerSelection;
     } else if (randomNumber === 2){
-        computerPick = weapons[1];
-        console.log("Computer picked: " + computerPick);
-        return computerPick;
+        computerSelection = weapons[1];
+        console.log("Computer picked: " + capitalize(computerSelection));
+        return computerSelection;
     } else if (randomNumber === 3){
-        computerPick = weapons[2];
-        console.log("Computer picked: " + computerPick);
-        return computerPick;
+        computerSelection = weapons[2];
+        console.log("Computer picked: " + capitalize(computerSelection));
+        return computerSelection;
     } else {
         console.log("Something went wrong, try again!");
     }
 }
 
 //Evaluates winner according to conditions and adds points to winning player
-function evaluateWinner(){
-    
-    chooseWeaponUser();
-    chooseWeaponComputer();
-    
-    if (userPick === "rock" && computerPick === "paper" ||
-        userPick === "paper" && computerPick === "scissors" ||
-        userPick === "scissors" && computerPick === "rock") {
-        console.log("Computer wins! " + computerPick + " beats " + userPick + "!");
-        return ++userPoints;
-    } else if (userPick === "rock" && computerPick === "scissors" ||
-               userPick === "paper" && computerPick === "rock" ||
-               userPick === "scissors" && computerPick === "paper") {
-        console.log("User wins! " + userPick + " beats " + computerPick + "!");
+function evaluateRoundWinner() {
+
+    if (playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("Computer wins! " + capitalize(computerSelection) + " beats " + capitalize(playerSelection) + "!");
+        return ++playerPoints;
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+               playerSelection === "paper" && computerSelection === "rock" ||
+               playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("User wins! " + capitalize(playerSelection) + " beats " + capitalize(computerSelection) + "!");
         return ++computerPoints
-    } else if (userPick === "rock" || computerPick === "rock" ||
-               userPick === "scissors" || computerPick === "scissors" ||
-               userPick === "paper" || computerPick === "paper"){
+    } else if (playerSelection === "rock" || computerSelection === "rock" ||
+               playerSelection === "scissors" || computerSelection === "scissors" ||
+               playerSelection === "paper" || computerSelection === "paper"){
+        console.log("It's a tie!");
+    }
+}
+
+//Plays one round and evaluates winner
+function playRound() {
+
+    playerPlay();
+    computerPlay();
+    evaluateRoundWinner();
+}
+
+//Resets all counters to 0, plays 5 rounds after which it announces the game winner
+function game(){
+
+    playerPoints = 0;
+    computerPoints = 0;
+
+    for (let i = 0; i < 5; i++){
+        playRound();
+        console.log("Computer points: " + computerPoints + "\n" + 
+                    "Player points: " + playerPoints);
+    }
+
+    if(playerPoints > computerPoints){
+        console.log("Player is the game winner!");
+    } else if (playerPoints < computerPoints) {
+        console.log("Computer is the game winner!");
+    } else {
         console.log("It's a tie!");
     }
 }
